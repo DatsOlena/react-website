4a9032a91324b54544eaf8c988b2161b84ddbfc8
Olena Dats <olena.dats@screeningeagle.com>
2023-11-04 13:19:17 +0100

    Fix behaviour and bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button';
@@ -9,7 +9,7 @@ function Navbar() {
   const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(true);
+  const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
     if(window.innerWidth <=960) {
@@ -19,13 +19,17 @@ function Navbar() {
     }
   }
 
+  useEffect(() => {
+    showButton();
+  }, [])
+
   window.addEventListener('resize', showButton);
 
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to='/' className='navbar-logo'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             TRVL <i className='fab fa-typo3' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
